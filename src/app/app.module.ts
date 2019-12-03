0196b0a7eeb512d50e16ceb72aa3735af53f6315
Camilo Arias <jariasar@everis.com>
2019-12-03 11:29:18 -0300

    subida de ejercicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VotesComponent } from './votes/votes.component';
+import { LibreriaComponent } from './ejercicios/libreria/libreria.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LibraryService } from './services/library.service';
+import { FormsComponent } from './ejercicios/forms/forms.component';
+import { CalculadoraComponent } from './ejercicios/calculadora/calculadora.component';
+import { CambioMonedaComponent } from './ejercicios/cambio-moneda/cambio-moneda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VotesComponent,
+    LibreriaComponent,
+    FormsComponent,
+    CalculadoraComponent,
+    CambioMonedaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LibraryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
